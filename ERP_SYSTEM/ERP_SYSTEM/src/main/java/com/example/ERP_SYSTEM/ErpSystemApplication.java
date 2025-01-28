638ffb8386e93ac0382eb34d93fabe6c586e68c2
Mahmoud Orabi <mahmoudor.de@gmail.com>
2025-01-28 16:35:15 +0100

    das Projekt ist ein ERP System

---
+++ ERP_SYSTEM/ERP_SYSTEM/src/main/java/com/example/ERP_SYSTEM/ErpSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.ERP_SYSTEM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpSystemApplication.class, args);
+	}
+
+}
---
+++ ERP_SYSTEM/ERP_SYSTEM/src/test/java/com/example/ERP_SYSTEM/ErpSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ERP_SYSTEM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
