49a4cdec66e82710602d28dae45a70a2a509f98a
Mahmoud Orabi <mahmoudor.de@gmail.com>
2025-02-01 16:32:39 +0100

    Role erstellt

--- ERP_SYSTEM/ERP_SYSTEM/src/main/java/com/example/ERP_SYSTEM/entities/Users/Role.java
+++ ERP_SYSTEM/ERP_SYSTEM/src/main/java/com/example/ERP_SYSTEM/entities/Users/Role.java
@@ -1,4 +1,68 @@
 package com.example.ERP_SYSTEM.entities.Users;
 
+
+
+
+import jakarta.persistence.*;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "role")
+
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Integer id;
+    @Column(name = "role_name", nullable = false, length = 255)
+    private String name;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "permission_role",
+            joinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "permission_id", referencedColumnName = "id"))
+    private List<Permission> permissionList = new ArrayList<>();
+
+
+    public Role() {
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Permission> getPermissionList() {
+        return permissionList;
+    }
+
+    public void setPermissionList(List<Permission> permissionList) {
+        this.permissionList = permissionList;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", permissionList=" + permissionList +
+                '}';
+    }
+
 }
